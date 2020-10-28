ebf82cafe69d3e7bc085e61a17d4f3ba263daa0c
Saikka <ekaterina.piispanen@gmail.com>
2020-10-28 23:40:38 +0200

    news editing

--- Hogwarts/controllers/news.js
+++ Hogwarts/controllers/news.js
@@ -46,3 +46,56 @@ exports.addNews = (req, res, next) => {
       next(err);
     });
 };
+
+exports.updateNews = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed');
+    error.statusCode = 422;
+    throw error;
+  }
+  const id = req.params.id;
+  News.findById(id)
+    .then((news) => {
+      if (!news) {
+        const error = new Error('Cound not find news!');
+        error.statusCode = 404;
+        throw error;
+      }
+      news.title = req.body.title;
+      news.content = req.body.content;
+      news.author = req.body.author;
+      return news.save();
+    })
+    .then((result) => {
+      res.status(200).json({ message: 'News updated', news: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.deleteNews = (req, res, next) => {
+  const id = req.params.id;
+  News.findById(id)
+    .then((news) => {
+      if (!news) {
+        const error = new Error('Cound not find news!');
+        error.statusCode = 404;
+        throw error;
+      }
+      return News.findByIdAndRemove(id);
+    })
+    .then(() => {
+      res.status(200).json({ message: 'News deleted', id: id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
--- Hogwarts/routes/news.js
+++ Hogwarts/routes/news.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get('/', newsController.getNews);
 
 router.post(
-  '/add-news',
+  '/add',
   [
     body('title').trim().isLength({ min: 5 }),
     body('content').trim().isLength({ min: 15 }),
@@ -17,5 +17,17 @@ router.post(
   ],
   newsController.addNews
 );
+
+router.put(
+  '/edit/:id',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 15 }),
+    body('author').trim().isLength({ min: 3 })
+  ],
+  newsController.updateNews
+);
+
+router.delete('/delete/:id', newsController.deleteNews);
 
 module.exports = router;
